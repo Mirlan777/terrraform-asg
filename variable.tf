e9c4c95f74cf983cabf24846c03c079c9a94d2a3
Mirlan777 <t.mirlan@yahoo.com>
2021-06-03 21:15:09 +0000

    asg

---
+++ variable.tf
@@ -0,0 +1,19 @@
+variable "desired_capacity" {
+    default    = 3
+}
+variable "max_size" {
+    default    = 3
+}
+
+variable "min_size" {
+    default    = 3
+}
+        
+        
+variable "instance_type" {
+    default    = "c5.large"
+}
+variable "region" {
+    default             = "us-east-1"
+}
+    
